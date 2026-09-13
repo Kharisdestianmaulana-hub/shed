a4d0d6746ed61581090631bddb581a84b7b5ad9f
Kharis <maulanakharis129@gmail.com>
2026-09-13 17:32:41 +0700

    Initial Commit

---
+++ Shed/ShedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShedApp.swift
+//  Shed
+//
+//  Created by Kharis Destian Maulana on 13/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ShedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
